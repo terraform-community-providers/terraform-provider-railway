97ec902174e4290747d0fe78374b26ba99132183
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-07-28 12:49:43 +0100

    Update docs [skip ci]

--- examples/resources/railway_custom_domain/resource.tf
+++ examples/resources/railway_custom_domain/resource.tf
@@ -1,5 +1,5 @@
 resource "railway_custom_domain" "api" {
-  repository     = "api.example.com"
+  domain         = "api.example.com"
   environment_id = railway_project.example.default_environment.id
   service_id     = railway_service.example.id
 }
