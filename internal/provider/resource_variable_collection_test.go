663091afd094c3a49e1ce2bc4e4ca391e9269396
Alex Povar <a.v.povar@gmail.com>
2024-05-17 18:19:19 +0500

    Full test set for variable collection

--- internal/provider/resource_variable_collection_test.go
+++ internal/provider/resource_variable_collection_test.go
@@ -9,6 +9,7 @@ import (
 
 func TestAccVariableCollectionResourceDefault(t *testing.T) {
 
+	environmentName := "production"
 	environmentId := "3050e612-087b-40dd-bdf2-4b52a8290900"
 	serviceId := "8faa58d7-8a06-4b9a-8e9a-4257c91c8b15"
 
@@ -26,6 +27,37 @@ func TestAccVariableCollectionResourceDefault(t *testing.T) {
 					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_A", "one"),
 					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_B", "two"),
 					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_C", "three"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "railway_variable_collection.test",
+				ImportState:       true,
+				ImportStateId:     fmt.Sprintf("%s:%s:VALUE_A:VALUE_B:VALUE_C", serviceId, environmentName),
+				ImportStateVerify: true,
+			},
+			// Update with default values
+			{
+				Config: testAccVariableCollectionResourceConfigDefault("one", "two", "three"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "id", fmt.Sprintf("%s:%s:VALUE_A:VALUE_B:VALUE_C", serviceId, environmentId)),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "environment_id", environmentId),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "service_id", serviceId),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_A", "one"),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_B", "two"),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_C", "three"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccVariableCollectionResourceConfigDefault("four", "five", "six"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "id", fmt.Sprintf("%s:%s:VALUE_A:VALUE_B:VALUE_C", serviceId, environmentId)),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "environment_id", environmentId),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "service_id", serviceId),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_A", "four"),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_B", "five"),
+					resource.TestCheckResourceAttr("railway_variable_collection.test", "variables.VALUE_C", "six"),
 				),
 			},
 		},
