4053c64df5f68e5a9f786d53dbbfee5df2de5547
Chandler Gonzales <jcgsville@gmail.com>
2025-10-20 22:31:52 -0700

    add environment_id to service resource

--- examples/resources/railway_service/resource.tf
+++ examples/resources/railway_service/resource.tf
@@ -2,3 +2,9 @@ resource "railway_service" "example" {
   name       = "api"
   project_id = railway_project.example.id
 }
+
+resource "railway_service" "with_environment" {
+  name           = "api"
+  project_id     = railway_project.example.id
+  environment_id = railway_environment.staging.id
+}
